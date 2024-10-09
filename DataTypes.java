79e24895683c21bf1ff15681c26664fbf3b69a22
Nilesh Namdev Tambekar <95668535+Nilesh10072003@users.noreply.github.com>
2024-10-09 13:11:06 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,33 @@
+package sample;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		byte myByte = 100;      // 8-bit signed integer
+        short myShort = 10000;  // 16-bit signed integer
+        int myInt = 100000;     // 32-bit signed integer
+        long myLong = 100000L;  // 64-bit signed integer
+
+        // Floating point types
+        float myFloat = 10.99f;  // 32-bit floating point
+        double myDouble = 99.99; // 64-bit floating point
+
+        // Character type
+        char myChar = 'A';       // 16-bit Unicode character
+
+        // Boolean type
+        boolean isJavaFun = true; // true or false
+
+        // Displaying the values
+        System.out.println("Byte: " + myByte);
+        System.out.println("Short: " + myShort);
+        System.out.println("Int: " + myInt);
+        System.out.println("Long: " + myLong);
+        System.out.println("Float: " + myFloat);
+        System.out.println("Double: " + myDouble);
+        System.out.println("Char: " + myChar);
+        System.out.println("Boolean: " + isJavaFun);
+
+	}
+
+}
