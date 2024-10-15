f193da05286f88f3e7cb86c2ffe581bcf97e5eaa
Nilesh Namdev Tambekar <95668535+Nilesh10072003@users.noreply.github.com>
2024-10-15 13:34:56 +0530

    Add files via upload

---
+++ RelationalOperator.java
@@ -0,0 +1,29 @@
+package sample;
+
+public class RelationalOperator {
+
+	public static void main(String[] args) {
+		int a = 10;
+        int b = 20;
+
+        // Equal to (==)
+        System.out.println("a == b: " + (a == b)); // Output: false
+
+        // Not equal to (!=)
+        System.out.println("a != b: " + (a != b)); // Output: true
+
+        // Greater than (>)
+        System.out.println("a > b: " + (a > b)); // Output: false
+
+        // Less than (<)
+        System.out.println("a < b: " + (a < b)); // Output: true
+
+        // Greater than or equal to (>=)
+        System.out.println("a >= b: " + (a >= b)); // Output: false
+
+        // Less than or equal to (<=)
+        System.out.println("a <= b: " + (a <= b)); // Output: tru
+
+	}
+
+}
