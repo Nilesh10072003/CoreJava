5ff3aac44320d117346f19516193807dff296598
Nilesh Namdev Tambekar <95668535+Nilesh10072003@users.noreply.github.com>
2024-10-28 13:56:25 +0530

    Add files via upload

---
+++ SquareCalculator.java
@@ -0,0 +1,20 @@
+package assingment;
+
+import java.util.Scanner;
+
+public class SquareCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the length of a side of the square: ");
+        double sideLength = scanner.nextDouble();
+
+        double area = sideLength * sideLength;
+        double perimeter = 4 * sideLength;
+
+        System.out.println("Area of the square: " + area);
+        System.out.println("Perimeter of the square: " + perimeter);
+
+        scanner.close();
+    }
+}
